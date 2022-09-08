68df34b64469b8f49632ef7a8fb524811bc878e2
Samokhin Alexander <icorwin74@gmail.com>
2022-09-08 15:29:34 +0300

    Create PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,12 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(null=True)
 
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    lat = models.DecimalField(max_digits=10, decimal_places=7)
+    lon = models.DecimalField(max_digits=10, decimal_places=7)
